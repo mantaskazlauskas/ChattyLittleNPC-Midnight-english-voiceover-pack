db3750757b22feca709696291e75b8ccce9c8562
Mantas Kazlauskas <mmantas.kazlauskas@gmail.com>
2025-12-14 20:51:16 +0200

    init commit

---
+++ ChattyLittleNpc_Midnight_voiceovers/Main.lua
@@ -0,0 +1,10 @@
+---@class ChattyLittleNpc_Midnight_voiceovers: table
+local ChattyLittleNpc_Midnight_voiceovers = {}
+_G["ChattyLittleNpc_Midnight_voiceovers"] = ChattyLittleNpc_Midnight_voiceovers
+
+ChattyLittleNpc_Midnight_voiceovers.Metadata = {
+    ContainsQuestVoiceovers = true,
+    ContainsGossipVoiceovers = true,
+    ContainsGameObjectVoiceovers = true,
+    ContainsItemVoiceovers = true
+}
